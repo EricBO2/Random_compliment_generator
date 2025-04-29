d21a6fa3714975b6c4319bad0b6b775becd498e5
EricBO2 <eric.carlsson@stud.sti.se>
2025-04-29 10:43:53 +0200

    Initial commit

---
+++ src/main/java/se/demo/lektion3/Main.java
@@ -0,0 +1,7 @@
+package se.demo.lektion3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/se/demo/lektion3/Random.java
@@ -0,0 +1,18 @@
+package se.demo.lektion3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Random {
+    List<String> complimentList = new ArrayList<String>();
+    public Random() {
+        complimentList.add("Nice hair");
+        complimentList.add("Nice face");
+        complimentList.add("Nice eyes");
+        complimentList.add("Nice legs");
+        complimentList.add("Nice mustache");
+    }
+
+
+
+}
